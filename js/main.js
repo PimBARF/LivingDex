90ef6436eb76726c4412f83c8470e941fe7a1072
Pim Jong <pimbarf@gmail.com>
2026-08-21 01:47:00 +0200

    Add manifest for installing on smartphones Add icons for smartphones

--- js/main.js
+++ js/main.js
@@ -138,3 +138,16 @@ async function initializeLivingDexApp() {
     showToast('LivingDex startup encountered a loading problem.', 'warning');
   }
 })();
+
+// Register Service Worker for PWA installability
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then((registration) => {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      })
+      .catch((err) => {
+        console.log('ServiceWorker registration failed: ', err);
+      });
+  });
+}
